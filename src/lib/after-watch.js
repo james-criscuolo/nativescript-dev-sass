f84b615aa4072cdb0ada8c192c60f03257a45eb1
fatme <hfatme@gmail.com>
2018-05-09 17:07:01 +0300

    Fix non-working Ctrl+c on windows

--- src/lib/after-watch.js
+++ src/lib/after-watch.js
@@ -1,9 +1,5 @@
 var converter = require('./converter');
 
 module.exports = function ($logger) {
-	var watcher = converter.getWatcher();
-	if (watcher) {
-		$logger.info("Stopping nativescript-dev-sass watcher");
-		watcher.close();
-	}
+	converter.dispose();
 }
